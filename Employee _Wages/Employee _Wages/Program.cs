6ee459e1f34688b51718d2497d82f97579bd92f1
jayantsaraf <sarafjayant24@gmail.com>
2020-09-26 16:19:38 +0530

    [jayant] Add. Calculate Wages function added

--- Employee _Wages/Employee _Wages/Program.cs
+++ Employee _Wages/Employee _Wages/Program.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 
-namespace Employee_Wages
+namespace UC2_Calculate_Employee_Wage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ////variables declaration
+            //constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variable
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            ////program working
+            //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
+            
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage = " + empWage);
         }
     }
 }
