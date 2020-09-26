7e6039ede442537210e407b17496ba0054370aac
jayantsaraf <sarafjayant24@gmail.com>
2020-09-26 16:30:19 +0530

    [jayant] Add. Added part time wages

--- Employee _Wages/Employee _Wages/Program.cs
+++ Employee _Wages/Employee _Wages/Program.cs
@@ -6,10 +6,10 @@ namespace UC2_Calculate_Employee_Wage
     {
         static void Main(string[] args)
         {
-            //constants
+            ////constants
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
-            //variable
+            ////variable
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
@@ -19,7 +19,11 @@ namespace UC2_Calculate_Employee_Wage
             {
                 empHrs = 8;
             }
-            
+            else
+            {
+                empHrs = 4;////Assuming part time hours = 4
+            }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wage = " + empWage);
         }
