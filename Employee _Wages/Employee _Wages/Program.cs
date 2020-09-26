14cb2c27da4dacdd4295039d6b98d5bd8ba1d274
jayantsaraf <sarafjayant24@gmail.com>
2020-09-26 17:01:42 +0530

    [jayant] Add. Calculated wages of a month

--- Employee _Wages/Employee _Wages/Program.cs
+++ Employee _Wages/Employee _Wages/Program.cs
@@ -9,26 +9,25 @@ namespace UC2_Calculate_Employee_Wage
             ////constants
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
+            int totalEmpHrs = 0;
             ////variable
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
-            switch (empCheck)
+            if (empCheck == IS_FULL_TIME)
             {
-            case 0:
-            empHrs = 4;
-            break; 
-            case 1:
-            empHrs = 8;
-            break;
-            default:
-            empHrs = 0;
-            break;
-        }
+                empHrs = 8;
+                totalEmpHrs += empHrs;
+            }
+            else
+            {
+                empHrs = 8;////Assuming part time hours = 8
+                totalEmpHrs += empHrs;
+            }
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+            empWage = totalEmpHrs * EMP_RATE_PER_HOUR * 20; ////No. of days in month = 20
             Console.WriteLine("Employee wage = " + empWage);
         }
     }
